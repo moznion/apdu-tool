594cac8356c96cf75f7d2cca402caf7131b0742b
moznion <moznion@mail.moznion.net>
2022-02-21 02:30:47 -0800

    fmt
    Signed-off-by: moznion <moznion@mail.moznion.net>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use ts_102_221::class::{ClassTypeForStandardLogicalChannels, new_standard_class, SecureMessagingIndicationForStandardLogicalChannels};
+use ts_102_221::class::{
+    new_standard_class, ClassTypeForStandardLogicalChannels,
+    SecureMessagingIndicationForStandardLogicalChannels,
+};
 use ts_102_221::command_apdu::new_command_apdu;
 use ts_102_221::instruction::{SelectFile, UpdateBinary};
 use ts_131_102::access_technology_identifier::new_access_technology_identifier;
@@ -8,25 +11,51 @@ use ts_131_102::identifier_providable::IdentifierProvidable;
 use ts_131_102::operator_controlled_plmn_selector_with_access_technology::new_operator_controlled_plmn_selector_with_access_technology;
 use ts_131_102::plmn::new_plmn;
 
-fn main() { // TODO be CLI tool
-    let plmns_info = [
-        (new_plmn(310, 410).unwrap(), new_access_technology_identifier(Vec::from([UTRAN, EUTRAN]), Vec::new()))
-    ];
+fn main() {
+    // TODO be CLI tool
+    let plmns_info = [(
+        new_plmn(310, 410).unwrap(),
+        new_access_technology_identifier(Vec::from([UTRAN, EUTRAN]), Vec::new()),
+    )];
     let oplmnwact = new_operator_controlled_plmn_selector_with_access_technology(&plmns_info);
 
     let identifier = oplmnwact.get_identifier();
-    let class = new_standard_class(ClassTypeForStandardLogicalChannels::ISOIEC7816_4, SecureMessagingIndicationForStandardLogicalChannels::NoSM, 0).unwrap();
-    let instruction = SelectFile{};
-    let apdu = new_command_apdu(&class, &instruction, 0x00, 0x04, Option::None, Option::Some(&identifier));
+    let class = new_standard_class(
+        ClassTypeForStandardLogicalChannels::ISOIEC7816_4,
+        SecureMessagingIndicationForStandardLogicalChannels::NoSM,
+        0,
+    )
+    .unwrap();
+    let instruction = SelectFile {};
+    let apdu = new_command_apdu(
+        &class,
+        &instruction,
+        0x00,
+        0x04,
+        Option::None,
+        Option::Some(&identifier),
+    );
     for b in apdu.to_bytes().unwrap() {
         print!("{:02x}", b);
     }
     println!();
 
     let oplmnwact_bytes = oplmnwact.to_bytes();
-    let class = new_standard_class(ClassTypeForStandardLogicalChannels::ISOIEC7816_4, SecureMessagingIndicationForStandardLogicalChannels::NoSM, 0).unwrap();
-    let instruction = UpdateBinary{};
-    let apdu = new_command_apdu(&class, &instruction, 0x00, 0x05, Option::None, Option::Some(oplmnwact_bytes.as_slice()));
+    let class = new_standard_class(
+        ClassTypeForStandardLogicalChannels::ISOIEC7816_4,
+        SecureMessagingIndicationForStandardLogicalChannels::NoSM,
+        0,
+    )
+    .unwrap();
+    let instruction = UpdateBinary {};
+    let apdu = new_command_apdu(
+        &class,
+        &instruction,
+        0x00,
+        0x05,
+        Option::None,
+        Option::Some(oplmnwact_bytes.as_slice()),
+    );
     for b in apdu.to_bytes().unwrap() {
         print!("{:02x}", b);
     }
